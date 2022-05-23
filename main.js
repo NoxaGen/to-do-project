d74a37974585e3ab5a26574853dbf229c1a57387
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-23 17:24:35 +0200

    quick refactor and test

--- main.js
+++ main.js
@@ -2,19 +2,16 @@ const userInput = document.querySelector('input');
 const addButton = document.querySelector('button');
 const tableUl = document.querySelector('.table ul')
 const taskArr = [];
-const deleteTask = document.querySelector()
+let deleteTask;
 let singleInput;
 
 const userValue = (e) => {
-    console.log(e.target.value)
+
     singleInput = e.target.value;
-
-
-
 }
 
 const createTask = (e) => {
-    console.log('working createTask');
+
     e.preventDefault();
     // taskArr.push(singleInput);
     const preTask = document.createElement('li');
@@ -25,8 +22,12 @@ const createTask = (e) => {
     preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
         '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
     tableUl.appendChild(preTask);
+    deleteTask = document.querySelectorAll('.table ul li .deleteTask');
+
 
 }
+
+
 
 userInput.addEventListener('input', userValue);
 
