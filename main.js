a75b73018c5bb2b4ff9abc79dbce05f9a51d7044
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-23 16:54:45 +0200

    add in function creating elements with buttons

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const userInput = document.querySelector('input');
 const addButton = document.querySelector('button');
+const tableUl = document.querySelector('.table ul')
+const taskArr = [];
+const deleteTask = document.querySelector()
 let singleInput;
 
 const userValue = (e) => {
     console.log(e.target.value)
     singleInput = e.target.value;
+
+
+
 }
 
 const createTask = (e) => {
     console.log('working createTask');
     e.preventDefault();
+    // taskArr.push(singleInput);
+    const preTask = document.createElement('li');
+    preTask.classList.add('activeLi');
+    preTask.textContent = singleInput;
+    // const liElement = document.querySelector('li:last-child');
+    // preTask.innerHTML = "<button class='taskDone'><i class='fas fa-check-circle'></button>" + preTask.textContent + "<button class='deleteTask'><i class='fas fa-minus-circle'></button>";
+    preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
+        '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
+    tableUl.appendChild(preTask);
 
 }
 
