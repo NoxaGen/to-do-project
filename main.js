99fa738580a1b65ce8e9554a8bbd31bfb88b33ee
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-24 15:19:39 +0200

    add event on remove button wich delete whole task

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const userInput = document.querySelector('input');
+let userInput = document.querySelector('input');
 const addButton = document.querySelector('button');
 const tableUl = document.querySelector('.table ul')
 const taskArr = [];
-let deleteTask;
+let deleteTasks = [];
 let singleInput;
 
 const userValue = (e) => {
@@ -13,21 +13,40 @@ const userValue = (e) => {
 const createTask = (e) => {
 
     e.preventDefault();
-    // taskArr.push(singleInput);
+    taskArr.push(singleInput);
     const preTask = document.createElement('li');
     preTask.classList.add('activeLi');
     preTask.textContent = singleInput;
-    // const liElement = document.querySelector('li:last-child');
-    // preTask.innerHTML = "<button class='taskDone'><i class='fas fa-check-circle'></button>" + preTask.textContent + "<button class='deleteTask'><i class='fas fa-minus-circle'></button>";
     preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
         '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
     tableUl.appendChild(preTask);
-    deleteTask = document.querySelectorAll('.table ul li .deleteTask');
+
+    deleteTasks = [...document.querySelectorAll('.table ul li .deleteTask')];
+
+    userInput.value = '';
+
+    deleteTasks.forEach(delTask => {
+        delTask.addEventListener('click', function () {
+            delTask.parentNode.remove();
+        });
+    });
+
 
 
 }
 
 
+// const taskRemover = (e) => {
+//     e.deleteTask.parentNode.remove();
+// }
+
+// deleteTasks.forEach(delTask => {
+//     delTask.addEventListener('click', function () {
+//         console.log('working')
+//     });
+// });
+
+// addEventListener('click', taskRemover);
 
 userInput.addEventListener('input', userValue);
 
