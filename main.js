d44662c2cb0be167c705e819afbef7e92ed3819a
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-24 15:33:12 +0200

    add event on task-done button wich change color

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const addButton = document.querySelector('button');
 const tableUl = document.querySelector('.table ul')
 const taskArr = [];
 let deleteTasks = [];
+let tasksDone = [];
 let singleInput;
+let tasksDoneCounter = 0;
 
 const userValue = (e) => {
 
@@ -20,16 +22,24 @@ const createTask = (e) => {
     preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
         '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
     tableUl.appendChild(preTask);
-
-    deleteTasks = [...document.querySelectorAll('.table ul li .deleteTask')];
-
     userInput.value = '';
 
+    deleteTasks = [...document.querySelectorAll('.table ul li .deleteTask')];
     deleteTasks.forEach(delTask => {
         delTask.addEventListener('click', function () {
             delTask.parentNode.remove();
         });
     });
+
+    tasksDone = [...document.querySelectorAll('.table ul li .taskDone')]
+    tasksDone.forEach(taskDone => {
+        taskDone.addEventListener('click', function () {
+            taskDone.parentNode.style.textDecoration = "line-through";
+            taskDone.parentNode.style.color = "grey";
+            taskDone.parentNode.style.backgroundColor = "rgba(172, 255, 47, 0.232)";
+            tasksDoneCounter++;
+        })
+    })
 
 
 
