b93edd17ae3ef4ce9b57837164aae22b559fbbf6
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-25 13:25:45 +0200

    add some comments and try to solve issue

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ let userInput = document.querySelector('input');
 const addButton = document.querySelector('button');
 const tableUl = document.querySelector('.table ul')
 let taskArr = []; //all elements on table will be pushed in this array
-let deleteTasks = []; //arr need for delete button comes from append childe with inner html
+let deleteTasks = []; //arr need for delete button comes from append childs with inner html
 let tasksDone = []; // same as upper array, its for task-done button 
 let completedTasks = []; //all completed tasks will be pushed into this arr
 let singleInput; //need this var for global use, it will store value of e.target
-
+let testcounter = 0;
 
 const userValue = (e) => {
 
     singleInput = e.target.value;
-    if (singleInput.length >= 25) {
-        return (alert('Zadanie może mieć maksymalnie 25 znaków.'))
+    if (singleInput.length >= 29) {
+        return (alert('Zadanie może mieć maksymalnie 29 znaków.'))
     }
 }
 
@@ -25,7 +25,12 @@ const createTask = (e) => {
     }
 
     e.preventDefault();
-    taskArr.push(singleInput);
+    // taskArr.push(singleInput);
+
+    // taskArr.forEach((task, key) => {
+    //     task.dataset.key = key;
+    // });
+
     const preTask = document.createElement('li');
     preTask.classList.add('activeLi');
     preTask.textContent = singleInput.toLowerCase();
@@ -34,15 +39,35 @@ const createTask = (e) => {
     preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
         '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
     tableUl.appendChild(preTask);
+
+    taskArr.push(preTask);
+
+    taskArr.forEach((task, key) => {
+        task.dataset.key = key;
+    });
+
     userInput.value = '';
     singleInput = '';
 
     deleteTasks = [...document.querySelectorAll('.table ul li .deleteTask')];
     deleteTasks.forEach(delTask => {
         delTask.addEventListener('click', function () {
-            delTask.parentNode.remove();
+            this.parentNode.remove();
+
+            const index = this.parentNode.dataset.key;
+            taskArr.splice(index, 1);
+
+
+
+
+
+
         });
+
+
     });
+
+    // do poprawy i sprawdzenia dlaczego sie zachowuje w ten sposob
 
     tasksDone = [...document.querySelectorAll('.table ul li .taskDone')]
     tasksDone.forEach(taskDone => {
@@ -52,7 +77,13 @@ const createTask = (e) => {
             taskDone.parentNode.style.color = "grey";
             taskDone.parentNode.style.backgroundColor = "rgba(172, 255, 47, 0.232)";
             completedTasks.push(taskDone.parentNode); //need this to count completed tasks
+            // completedTasks.push(e.target);
             taskDone.disabled = "disabled"; //after one click button stop duplicate push
+            // testcounter++;
+
+            // upper command doesnt working! still counting from last element...
+            //its working in other way but older elements beneath still adding into array
+
         })
     })
 }
