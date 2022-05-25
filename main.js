cf9293d263d515930a4b469739b33f6fc068fa52
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-25 16:29:30 +0200

    get rid of usless comments and do some refactor

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
 let userInput = document.querySelector('input');
 const addButton = document.querySelector('button');
 const tableUl = document.querySelector('.table ul')
-let taskArr = [];
+let tasksOnTable = [];
 let completedTasks = [];
 let singleInput; //need this var for global use, it will store value of input
 
+//functions
 
-
-//function get input value and pass it into global var
 const userValue = (e) => {
     singleInput = e.target.value;
     if (singleInput.length >= 29) {
         return (alert('Zadanie może mieć maksymalnie 29 znaków.'))
     }
 }
-
-// function add event on clicked check-button on the todo list
 const completeTask = (e) => {
     e.currentTarget.parentElement.style.textDecoration = "line-through";
     e.currentTarget.parentElement.style.color = "grey";
     e.currentTarget.parentElement.style.backgroundColor = "rgba(172, 255, 47, 0.232)";
     completedTasks.push(e.currentTarget.parentElement);
-    e.currentTarget.disabled = "disabled";
+    e.currentTarget.disabled = "disabled"; //block possibility of multiply clicking same button
     console.log(e.currentTarget)
 }
 
-// const removeTask = (e) => {
-//    e.target.parentNode.remove();
-// const index = e.target.parentNode.dataset.key;
-//         taskArr.splice(index, 1);
-// }
+const removeTask = (e) => {
+    e.currentTarget.parentNode.remove();
+    const index = e.currentTarget.parentNode.dataset.key;
+    tasksOnTable.splice(index, 1);
+}
 
 const createTask = (e) => {
 
@@ -38,32 +35,23 @@ const createTask = (e) => {
         return (alert("Pole nie może być puste."))
     }
     e.preventDefault();
-
     const preTask = document.createElement('li');
     preTask.classList.add('activeLi');
     preTask.textContent = singleInput.toLowerCase();
     preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
         '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
     tableUl.appendChild(preTask);
-    taskArr.push(preTask);
-    taskArr.forEach((task, key) => {
+    tasksOnTable.push(preTask);
+    tasksOnTable.forEach((task, key) => {
         task.dataset.key = key;
     });
     userInput.value = '';
     singleInput = '';
 
-    preTask.querySelector('.deleteTask').addEventListener('click', function () {
-        this.parentNode.remove();
-
-        const index = this.parentNode.dataset.key;
-        taskArr.splice(index, 1);
-    })
-
+    preTask.querySelector('.deleteTask').addEventListener('click', removeTask);
     preTask.querySelector('.taskDone').addEventListener('click', completeTask);
-
-
 }
 
+//controls
 userInput.addEventListener('input', userValue);
-
 addButton.addEventListener('click', createTask);
