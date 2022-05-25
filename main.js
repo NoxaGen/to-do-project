68fde64133284cdeec8bf266f277b3eab71a491f
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-05-25 19:39:49 +0200

    fixed both functions, now they work correctly

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let userInput = document.querySelector('input');
 const addButton = document.querySelector('button');
-const tableUl = document.querySelector('.table ul')
+const tableUl = document.querySelector('.table ul');
+let userInput = document.querySelector('input');
 let tasksOnTable = [];
 let completedTasks = [];
 let singleInput; //need this var for global use, it will store value of input
@@ -14,22 +14,25 @@ const userValue = (e) => {
     }
 }
 const completeTask = (e) => {
-    e.currentTarget.parentElement.style.textDecoration = "line-through";
-    e.currentTarget.parentElement.style.color = "grey";
-    e.currentTarget.parentElement.style.backgroundColor = "rgba(172, 255, 47, 0.232)";
-    completedTasks.push(e.currentTarget.parentElement);
+    e.currentTarget.parentNode.style.textDecoration = "line-through";
+    e.currentTarget.parentNode.style.color = "grey";
+    e.currentTarget.parentNode.style.backgroundColor = "rgba(172, 255, 47, 0.232)";
+    completedTasks.push(e.currentTarget.parentNode);
     e.currentTarget.disabled = "disabled"; //block possibility of multiply clicking same button
-    console.log(e.currentTarget)
 }
 
 const removeTask = (e) => {
     e.currentTarget.parentNode.remove();
     const index = e.currentTarget.parentNode.dataset.key;
     tasksOnTable.splice(index, 1);
+    console.log(index)
+    console.log(tasksOnTable)
+    tasksOnTable.forEach((task, key) => {
+        task.dataset = key;
+    });
 }
 
 const createTask = (e) => {
-
     if (!singleInput) {
         e.preventDefault();
         return (alert("Pole nie może być puste."))
@@ -40,11 +43,12 @@ const createTask = (e) => {
     preTask.textContent = singleInput.toLowerCase();
     preTask.innerHTML = '<button class="taskDone"><i class="fas fa-check-circle"></i></button>' + preTask.textContent +
         '<button class="deleteTask"><i class="fas fa-minus-circle"></i></button>';
-    tableUl.appendChild(preTask);
+    tableUl.appendChild(preTask)
     tasksOnTable.push(preTask);
     tasksOnTable.forEach((task, key) => {
-        task.dataset.key = key;
+        task.dataset = key;
     });
+
     userInput.value = '';
     singleInput = '';
 
